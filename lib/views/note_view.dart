2841639166582497902997c4ef222087a7e9415d
Kaream <kareamtanany@gmail.com>
2024-10-16 12:45:11 +0300

    generate type adapter file

--- lib/views/note_view.dart
+++ lib/views/note_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/widgets/Custom_bottom_sheet.dart';
+import 'package:note_app/widgets/custom_bottom_sheet.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
